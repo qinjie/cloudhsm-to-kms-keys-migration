c6ff0a4de914bc8a8ef9d840283645d8da1d6e08
Qinjie <mark.qj@gmail.com>
2025-07-14 09:20:40 +0800

    Generate both ECC and RSA test keys
    Include RSA keys.

--- generate-cloudhsm-test-keys.sh
+++ generate-cloudhsm-test-keys.sh
@@ -1,9 +1,17 @@
 for i in {1..20}; do
 	cloudhsm-cli key generate-asymmetric-pair ec \
-		--public-label "mvgx-pub-$i" \
-		--private-label "mvgx-priv-$i" \
+		--public-label "mvgx-ec-pub-$i" \
+		--private-label "mvgx-ec-priv-$i" \
 		--curve secp256k1 \
 		--public-attributes encrypt=true verify=true \
 		--private-attributes decrypt=true sign=true 
+done
+
+for i in {1..20}; do
+  cloudhsm-cli key generate-asymmetric-pair rsa \
+    --public-label "mvgx-rsa-pub-$i" \
+    --private-label "mvgx-rsa-priv-$i" \
+    --modulus-size-bits 2048 \
+    --public-exponent 65537;
 done
 
